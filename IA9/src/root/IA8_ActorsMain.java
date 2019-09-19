b188b26013a6d28b6ab992f38ecf9aeca45e6a23
Benjamin Lehmann <bclehmann@yahoo.ca>
2019-09-18 18:13:26 -0600

    IA9

--- IA9/src/root/IA8_ActorsMain.java
+++ IA9/src/root/IA8_ActorsMain.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 
 public class IA8_ActorsMain {
 
+	static enum options {
+		SEARCH_BY_LASTNAME, SEARCH_BY_ROLE, LIST_PROMO, LIST_ON_SET, PRINT_FIRSTNAMES, PRINT_ROLES, EXIT, SEARCH_BY_FIRSTNAME
+	}
+
 	public static void main(String[] args) {
 		// System.out.println(System.getProperty("user.dir"));
 		String[] fileContents = Files.loadStringArr("HamletActors.txt");
@@ -44,29 +48,57 @@ public class IA8_ActorsMain {
 			System.out.println("MENU");
 			System.out.println("----------------");
 			System.out.println("1: Seach by last name");
-			System.out.println("2: Seach by role");
-			System.out.println("3: List promotional tour");
-			System.out.println("4: List actors currently on set");
-			System.out.println("5: Exit");
+			System.out.println("2: Seach by first name");
+			System.out.println("3: Seach by role");
+			System.out.println("4: List promotional tour");
+			System.out.println("5: List actors currently on set");
+			System.out.println("6: Print all first names");
+			System.out.println("7: Print all roles");
+			System.out.println("8: Exit");
+
+			options[] optionsArr = { 
+					options.SEARCH_BY_LASTNAME,
+					options.SEARCH_BY_FIRSTNAME,
+					options.SEARCH_BY_ROLE,
+					options.LIST_PROMO,
+					options.LIST_ON_SET,
+					options.PRINT_FIRSTNAMES,
+					options.PRINT_ROLES,
+					options.EXIT
+			};
 
 			int wanted = numscan.nextInt();
-			switch (wanted) {
-			case 1:
+			switch (optionsArr[wanted - 1]) {
+			case SEARCH_BY_LASTNAME:
 				System.out.println("What is the actor's last name?");
-				searchLastName(wordscan.nextLine(), actorList);
+				searchByLastName(wordscan.nextLine(), actorList);
 				break;
-			case 2:
+			case SEARCH_BY_ROLE:
 				System.out.println("What is the actor's role?");
-				searchRole(wordscan.nextLine(), actorList);
+				searchByRole(wordscan.nextLine(), actorList);
 				break;
-			case 3:
+			case LIST_PROMO:
 				listPromoTour(actorList);
 				break;
-			case 4:
+			case LIST_ON_SET:
 				System.out.println("What shooting day?");
 				listActorsOnSet(numscan.nextInt(), actorList);
 				break;
-			case 5:
+			case PRINT_FIRSTNAMES:
+				for (Actor curr : sortByFirstName(actorList)) {
+					System.out.println("--" + curr.firstName);
+				}
+				break;
+			case PRINT_ROLES:
+				for (Actor curr : sortByRole(actorList)) {
+					System.out.println("--" + curr.role);
+				}
+				break;
+			case SEARCH_BY_FIRSTNAME:
+				System.out.println("What is the actor's first name?");
+				searchByFirstName(wordscan.nextLine(), actorList);
+				break;
+			case EXIT:
 				keepLooping = false;
 				break;
 			}
@@ -75,20 +107,42 @@ public class IA8_ActorsMain {
 
 	}
 
-	private static void searchRole(String role, ArrayList<Actor> actorList) {
-		int middle = actorList.size() / 2;
+	private static void searchByRole(String role, ArrayList<Actor> actorList) {
+		ArrayList<Actor> sortedList= sortByRole((ArrayList<Actor>) actorList.clone());
+		int middle = sortedList.size() / 2;
 		while (true) {
-			Actor middleActor = actorList.get(middle);
+			Actor middleActor = sortedList.get(middle);
 			if (middleActor.role.equalsIgnoreCase(role)) {
-				System.out.println(middleActor);
+				System.out.println("--" + middleActor);
 				return;
 			} else if ((int) middleActor.role.charAt(0) < (int) role.charAt(0)) {
-				middle += (int) Math.ceil((actorList.size() - middle) / 2.0);
+				middle += (int) Math.ceil((sortedList.size() - middle) / 2.0);
 			} else {
 				middle /= 2;
 			}
 
-			if (middle < 0 || middle == actorList.size()) {
+			if (middle < 0 || middle == sortedList.size()) {
+				System.out.println("Not found");
+				return;
+			}
+		}
+	}
+	
+	private static void searchByFirstName(String firstName, ArrayList<Actor> actorList) {
+		ArrayList<Actor> sortedList= sortByFirstName((ArrayList<Actor>) actorList.clone());
+		int middle = sortedList.size() / 2;
+		while (true) {
+			Actor middleActor = sortedList.get(middle);
+			if (middleActor.firstName.equalsIgnoreCase(firstName)) {
+				System.out.println("--" + middleActor);
+				return;
+			} else if (shouldSwap(middleActor.firstName, firstName)) {
+				middle += (int) Math.ceil((sortedList.size() - middle) / 2.0);
+			} else {
+				middle /= 2;
+			}
+
+			if (middle < 0 || middle == sortedList.size()) {
 				System.out.println("Not found");
 				return;
 			}
@@ -96,30 +150,90 @@ public class IA8_ActorsMain {
 	}
 
 	private static void listActorsOnSet(int shootingDay, ArrayList<Actor> actorList) {
-		for(Actor curr: actorList) {
-			if(curr.dayShooting == shootingDay) {
-				System.out.println(curr);
+		for (Actor curr : actorList) {
+			if (curr.dayShooting == shootingDay) {
+				System.out.println("--" + curr);
 			}
 		}
 	}
 
 	private static void listPromoTour(ArrayList<Actor> actorList) {
-		for(Actor curr: actorList) {
-			if(curr.promoTeam) {
-				System.out.println(curr);
+		for (Actor curr : actorList) {
+			if (curr.promoTeam) {
+				System.out.println("--" + curr);
 			}
 		}
 	}
 
-	private static void searchLastName(String lastName, ArrayList<Actor> actorList) {
+	private static void searchByLastName(String lastName, ArrayList<Actor> actorList) {
 		for (Actor curr : actorList) {
 			if (curr.lastName.equalsIgnoreCase(lastName)) {
-				System.out.println(curr);
+				System.out.println("--" + curr);
 				return;
 			}
 		}
 
 		System.out.println("Not found");
+	}
+
+	@SuppressWarnings("unchecked")
+	private static ArrayList<Actor> sortByFirstName(ArrayList<Actor> actorList) {
+		ArrayList<Actor> outputList = (ArrayList<Actor>) actorList.clone();
+		for (int i = 0; i < outputList.size(); i++) {
+			int minimum = i;
+			for (int j = i + 1; j < outputList.size(); j++) {
+				if (shouldSwap(outputList.get(j).firstName, outputList.get(minimum).firstName)) {
+					minimum = j;
+				}
+			}
+
+			if (minimum != i) {
+				Actor temp = outputList.get(i);
+				outputList.set(i, outputList.get(minimum));
+				outputList.set(minimum, temp);
+			}
+		}
+
+		return outputList;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static ArrayList<Actor> sortByRole(ArrayList<Actor> actorList) {
+		ArrayList<Actor> outputList = (ArrayList<Actor>) actorList.clone();
+		for (int i = 0; i < outputList.size(); i++) {
+			int minimum = i;
+			for (int j = i + 1; j < outputList.size(); j++) {
+				if (shouldSwap(outputList.get(j).role, outputList.get(minimum).role)) {
+					minimum = j;
+				}
+			}
+
+			if (minimum != i) {
+				Actor temp = outputList.get(i);
+				outputList.set(i, outputList.get(minimum));
+				outputList.set(minimum, temp);
+			}
+		}
+
+		return outputList;
+	}
+
+	private static boolean shouldSwap(String a, String b) {
+		a = a.toUpperCase();
+		b = b.toUpperCase();
+		for (int i = 0; i < a.length(); i++) {
+			if (i == b.length()) {
+				return true;
+			}
+
+			if (b.charAt(i) > a.charAt(i)) {
+				return true;
+			} else if (b.charAt(i) < a.charAt(i)) {
+				return false;
+			}
+		}
+
+		return false;
 	}
 
 }
